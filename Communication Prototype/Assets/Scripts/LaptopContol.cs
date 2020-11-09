a74f8ba5c07015e922672fad3c91df1798ae355c
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-11-09 12:11:21 +0000

    Basic UI
    Added basic in world UI to laptop to make it hvae functionality/ different screens - J

---
+++ Communication Prototype/Assets/Scripts/LaptopContol.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaptopContol : MonoBehaviour
+{
+    public Button resources;
+    public Button map;
+    public Button messages;
+
+    public Button back;
+
+    public void Start()
+    {
+        resources.gameObject.SetActive(true);
+        map.gameObject.SetActive(true);
+        messages.gameObject.SetActive(true);
+        
+        back.gameObject.SetActive(false);
+    }
+
+    public void ButtonClicked()
+    {
+        resources.gameObject.SetActive(false);
+        map.gameObject.SetActive(false);
+        messages.gameObject.SetActive(false);
+        
+        back.gameObject.SetActive(true);
+    }
+
+    public void BackClicked()
+    {
+        resources.gameObject.SetActive(true);
+        map.gameObject.SetActive(true);
+        messages.gameObject.SetActive(true);
+        
+        back.gameObject.SetActive(false);
+    }
+}
