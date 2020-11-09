c65872edb4b55ddf64028777218435c52ffb6fcb
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-11-09 15:54:52 +0000

    Pop up text
    Added context sensitive text for interactons - J

---
+++ Communication Prototype/Assets/Scripts/SpotlightInteract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpotlightInteract : MonoBehaviour
+{
+    public Text interact;
+    void Start()
+    {
+        interact.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            interact.gameObject.SetActive(true);
+            Debug.Log("collide");
+        }
+    }
+}
