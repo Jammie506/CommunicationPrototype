67ec6b3a43019b6ca9c8481066d112652bd030ce
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2020-11-10 00:27:14 +0000

    More Assets
    Added more assets and created a rendering system for the grass

--- Communication Prototype/Assets/InstantiateGrass.cs
+++ Communication Prototype/Assets/InstantiateGrass.cs
@@ -4,24 +4,130 @@ using UnityEngine;
 
 public class InstantiateGrass : MonoBehaviour
 {
+    public GameObject player;
     public GameObject grass;
+    public GameObject tree;
+    public GameObject boulder;
+
+    public List<GameObject> grassList = new List<GameObject>();
+
+    public int grassLoops = 15;
+    public int treeIterations = 10;
+    public int boulderIterations = 5;
 
     void Start()
     {
-        createGrass();
+        CreateGrass(grassLoops,15, new Vector3(0f,0.6f,0f),15);
+        CreateTree(treeIterations, 0, new Vector3(0f, 0f, 0f), 35);
+        CreateBoulder(boulderIterations, 1, new Vector3(0f, 1f, 0f), 35);
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void createGrass()
+    private void Update()
     {
-        for (int i = -10; i < 10; i++)
+        foreach(GameObject g in grassList)
         {
-            for (int j = -10; j < 10; j++)
+            Vector3 d = player.transform.position - g.transform.position;
+            if(d.magnitude < 70)
             {
-                float randomValueX = Random.Range(-50, 50);
-                float randomValueY = Random.Range(-50, 50);
-                grass.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
-                grass.transform.localScale = new Vector3(Random.Range(0.2f, 1.9f), Random.Range(0.5f, 1f), Random.Range(0.25f, 1.5f));
-                Instantiate(grass, new Vector3(i + randomValueX, 0.5f, j + randomValueY), grass.transform.rotation);
+                g.SetActive(true);
+            }
+            else
+            {
+                g.SetActive(false);
+            }
+        }
+    }
+    public void CreateGrass(int num,int num2, Vector3 point, int radius)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            radius += Random.Range(1,6);
+            num2 += 1;
+            for (int j = 0; j < num2; j ++)
+            {
+                /* Distance around the circle */
+                var radians = 2 * Mathf.PI / num2 * j;
+
+                /* Get the vector direction */
+                var vertical = Mathf.Sin(radians);
+                var horizontal = Mathf.Cos(radians);
+
+                float randomX = Random.Range(-0.2f, 0.2f);
+                float randomY = Random.Range(-0.2f, 0.2f);
+                var spawnDir = new Vector3(horizontal + randomX, 0f, vertical + randomY);
+
+                /* Get the spawn position */
+                var spawnPos = point + spawnDir * radius ; // Radius is just the distance away from the point
+
+                 grass.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+                 grass.transform.localScale = new Vector3(Random.Range(0.4f, 1.2f), Random.Range(0.4f, 1.2f), Random.Range(0.4f, 1.2f));
+
+                /* Now spawn */
+                var enemy = Instantiate(grass, spawnPos, grass.transform.rotation) as GameObject;
+                grassList.Add(enemy);
+            }
+        }
+    }
+
+    public void CreateTree(int num, int num2, Vector3 point, int radius)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            radius += Random.Range(8, 12);
+            num2 += 1;
+            for (int j = 0; j < num2; j++)
+            {
+                /* Distance around the circle */
+                var radians = 2 * Mathf.PI / num2 * j;
+
+                /* Get the vector direction */
+                var vertical = Mathf.Sin(radians);
+                var horizontal = Mathf.Cos(radians);
+
+                float randomX = Random.Range(-0.3f, 0.3f);
+                float randomY = Random.Range(-0.3f, 0.3f);
+                var spawnDir = new Vector3(horizontal + randomX, 0f, vertical + randomY);
+
+                /* Get the spawn position */
+                var spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
+
+                tree.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+                tree.transform.localScale = new Vector3(Random.Range(1f, 1.5f), Random.Range(1f, 1.5f), Random.Range(1f, 1.5f));
+
+                /* Now spawn */
+                var enemy = Instantiate(tree, spawnPos, tree.transform.rotation) as GameObject;
+            }
+        }
+    }
+    public void CreateBoulder(int num, int num2, Vector3 point, int radius)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            radius += Random.Range(15, 25);
+            num2 += 2;
+            for (int j = 0; j < num2; j++)
+            {
+                /* Distance around the circle */
+                var radians = 2 * Mathf.PI / num2 * j;
+
+                /* Get the vector direction */
+                var vertical = Mathf.Sin(radians);
+                var horizontal = Mathf.Cos(radians);
+
+                float randomX = Random.Range(-0.2f, 0.2f);
+                float randomY = Random.Range(-0.2f, 0.2f);
+                var spawnDir = new Vector3(horizontal + randomX, 0f, vertical + randomY);
+
+                /* Get the spawn position */
+                var spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
+
+                boulder.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+                boulder.transform.localScale = new Vector3(Random.Range(1f, 1.2f), Random.Range(1f, 1.2f), Random.Range(1f, 1.2f));
+
+                /* Now spawn */
+                var enemy = Instantiate(boulder, spawnPos, boulder.transform.rotation) as GameObject;
             }
         }
     }
