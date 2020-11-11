848cea308f9f488b7024995c24086335c1ab7481
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2020-11-11 10:08:33 +0000

    Fixed small inventory problem

--- Communication Prototype/Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Communication Prototype/Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    public Transform targetPosition;
 
     Vector3 velocity;
     private bool isGrounded;
@@ -21,7 +22,10 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private UI_Inventory uiInventory;
 
+    private void OnEnable()
+    {
 
+    }
     private void Awake()
     {
         inventory = new Inventory();
@@ -30,7 +34,10 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Start()
     {
+        ItemAssets.Instance.pfItemWorld[0].GetComponent<BoxCollider>().enabled = true;
 
+
+        //CreateGrass(1, Vector3.zero, 10);
         ItemWorld.SpawnItemWorld(new Vector3(Random.Range(-15, 15), 0.25f, Random.Range(-16, 16)), new Item { itemType = Item.ItemType.Torch, amount = 1 });
         ItemWorld.SpawnItemWorld(new Vector3(Random.Range(-15, 15), 0.5f, Random.Range(-16, 16)), new Item { itemType = Item.ItemType.Key, amount = 1 });
         ItemWorld.SpawnItemWorld(new Vector3(Random.Range(-15, 15), 0.5f, Random.Range(-16, 16)), new Item { itemType = Item.ItemType.Note, amount = 1 });
@@ -38,19 +45,40 @@ public class PlayerMovement : MonoBehaviour
     private void OnTriggerEnter(Collider collision)
     {
         ItemWorld itemWorld = collision.GetComponent<ItemWorld>();
-        if(itemWorld != null)
+        if (itemWorld != null)
         {
             //If the colliding object contains Items world script
             inventory.AddItem(itemWorld.GetItem());
             itemWorld.DestroySelf();
         }
     }
+    public void CreateGrass(int num, Vector3 point, int radius)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            radius += Random.Range(1, 6);
+           
+
+            /* Distance around the circle */
+            var radians = 2 * Mathf.PI / num * i;
+
+            /* Get the vector direction */
+            var vertical = Mathf.Sin(radians);
+            var horizontal = Mathf.Cos(radians);
+
+            float randomX = Random.Range(-0.2f, 0.2f);
+            float randomY = Random.Range(-0.2f, 0.2f);
+            var spawnDir = new Vector3(horizontal + randomX, 0f, vertical + randomY);
+
+            ItemWorld.SpawnItemWorld(spawnDir, new Item { itemType = Item.ItemType.Torch, amount = 1 });
+        }
+    }
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         //Creates a sphere that returns true if collides with an object it wants to detect
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -64,10 +92,12 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded)
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-      
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        ItemAssets.Instance.pfItemWorld[0].transform.position = targetPosition.transform.position;
 
     }
 }
